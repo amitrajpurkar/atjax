93f573cad1582ec1a3fbf5b2055bea88f14e3999
Amit Rajpurkar <amitrajpurkar@gmail.com>
2020-08-19 17:30:02 -0400

    adding working example for circuit-breaker and aspect usage

--- springbackendsvc/src/main/java/com/anr/common/SBUtil.java
+++ springbackendsvc/src/main/java/com/anr/common/SBUtil.java
@@ -10,9 +10,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 
 import com.anr.config.ConfigProperties;
+import com.anr.exception.ErrorRootElement;
+import com.anr.exception.SBNestedException;
 import com.google.gson.Gson;
+import com.mongodb.MongoException;
+import com.mongodb.MongoSecurityException;
+import com.mongodb.MongoSocketReadTimeoutException;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 
 @Component
 public class SBUtil {
@@ -26,6 +34,7 @@ public class SBUtil {
     private static final String LOG_MSG_FORMAT = "[transmissionID = %s] %s (ts=%s)%n";
     private static final String DATE_FORMAT_WITH_MS = "yyyy-MM-dd HH:mm:ss.SSS";
     private static final String SPACE = " ";
+    private static final String ERR_MSG_SUFFIX = " at method: %s";
 
     public void logDebug(String transactionID, String message) {
         if (logger.isDebugEnabled()) {
@@ -79,5 +88,49 @@ public class SBUtil {
         rootCauseMsg = rootCauseMsg + SPACE + rootExcept.getStackTrace()[0].toString();
 
         return rootCauseMsg;
+    }
+
+    public ErrorRootElement parseHystrixException(String transactionID, Throwable e, String callingMethodName) {
+        String errMsgString = null;
+        ErrorRootElement err = new ErrorRootElement("ERR-000",
+                getRootCauseMessage(e) + String.format(ERR_MSG_SUFFIX, callingMethodName));
+        SBNestedException ne = new SBNestedException("caught in hystrix fallback", e);
+        if (ne.contains(MongoException.class) || ne.contains(MongoSecurityException.class)
+                || ne.contains(MongoSocketReadTimeoutException.class)) {
+            err.setErrorCode("ERR-MONGO");
+        } else if (ne.contains(RestClientException.class)) {
+            err.setErrorCode("ERR-RESTBACKEND");
+        }
+
+        if (e instanceof HystrixRuntimeException) {
+            switch (((HystrixRuntimeException) e).getFailureType()) {
+            case SHORTCIRCUIT:
+            case COMMAND_EXCEPTION:
+            case BAD_REQUEST_EXCEPTION:
+                errMsgString = "Hystrix runtime failure " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+                logStackTrace(transactionID, errMsgString, e);
+                break;
+            case TIMEOUT:
+                errMsgString = "Hystrix timeout " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+                logStackTrace(transactionID, errMsgString, e);
+                break;
+            case REJECTED_THREAD_EXECUTION:
+                errMsgString = "Hystrix rejects " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+                logStackTrace(transactionID, errMsgString, e);
+                break;
+            default:
+                errMsgString = "Hystrix something else " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+                logStackTrace(transactionID, errMsgString, e);
+                break;
+            }
+        } else if (e instanceof HystrixTimeoutException) {
+            errMsgString = "Hystrix timeout " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+            logStackTrace(transactionID, errMsgString, e);
+        } else {
+            errMsgString = "Not a hystrix failure " + String.format(ERR_MSG_SUFFIX, callingMethodName);
+            logStackTrace(transactionID, errMsgString, e);
+        }
+
+        return err;
     }
 }
--- springbackendsvc/src/main/java/com/anr/logging/ControllerLoggingAspect.java
+++ springbackendsvc/src/main/java/com/anr/logging/ControllerLoggingAspect.java
@@ -1,5 +1,94 @@
 package com.anr.logging;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.anr.common.SBUtil;
+import com.anr.config.ConfigProperties;
+import com.anr.controller.ControllerFailureResponses;
+import com.anr.model.SBResponseModel;
+import com.google.gson.Gson;
+import com.netflix.hystrix.HystrixCommand;
+
+@Aspect
+@Component
 public class ControllerLoggingAspect {
+    @Autowired
+    private ConfigProperties appProps;
+
+    @Autowired
+    private SBUtil sbutil;
+
+    @Autowired
+    private Gson gson;
+
+    @Autowired
+    private ControllerFailureResponses failures;
+
+    @Autowired
+    @Qualifier("CmdConfigDefService")
+    private HystrixCommand.Setter cmdConfigDefaultSvc;
+
+    private static final String SPACE = " ";
+
+    @Around("execution(* com.anr.controller.MainSBController.getSampleResponse(..)) "
+            + "&&args(transactionID,sourceChannel,locale,field1,field2,..)")
+    public SBResponseModel logSampleResponse(ProceedingJoinPoint jointpoint, String transactionID, String sourceChannel,
+            String locale, String field1, String field2) {
+        long startTime = System.currentTimeMillis();
+        // construct a logBuilder event
+        sbutil.logInfo(transactionID, "start time:" + startTime);
+
+        HystrixCommand<SBResponseModel> command = new HystrixCommand<SBResponseModel>(cmdConfigDefaultSvc) {
+            @Override
+            protected SBResponseModel run() throws Exception {
+                try {
+                    return (SBResponseModel) jointpoint.proceed();
+                } catch (Exception e) {
+                    throw e;
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            protected SBResponseModel getFallback() {
+                Throwable t = getExecutionException();
+
+                Signature signature = jointpoint.getSignature();
+                String methodName = signature.getName();
+                String transactionID = (String) jointpoint.getArgs()[0];
+                sbutil.logError(transactionID, String.format("Execution exception: (method: %s) %s", methodName,
+                        sbutil.getRootCauseMessage(t)));
+                sbutil.logStackTrace(transactionID, methodName, t);
+
+                return failures.getSampleFailureResponse(transactionID, sourceChannel, locale, field1, field2, t);
+            }
+        };
+        SBResponseModel response = command.execute();
+
+        String messageString = null;
+        if (response == null) {
+            messageString = "null response for default-service";
+        } else if (response.getErr() != null) {
+            StringBuilder errMsg = new StringBuilder();
+            errMsg.append(response.getErr().getMessage());
+            errMsg.append(SPACE);
+            errMsg.append(response.getErr().getTechMessage());
+            messageString = "Failure: " + errMsg.toString();
+        } else {
+            messageString = "Success: " + gson.toJson(response);
+        }
+
+        messageString += "; timetaken = " + (System.currentTimeMillis() - startTime) + " ms";
+        sbutil.logInfo(transactionID, messageString);
+
+        return response;
+    }
 
 }
