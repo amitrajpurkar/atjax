7a588cecbe6bce9654897c917b2b72166de714cb
Amit Rajpurkar <amitrajpurkar@gmail.com>
2020-08-23 17:50:24 -0400

    adding unit tests and trying to wire in jacoco

--- springbackendsvc/src/test/java/com/anr/BootstrapTests.java
+++ springbackendsvc/src/test/java/com/anr/BootstrapTests.java
@@ -3,11 +3,12 @@ package com.anr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//@Ignore
 @SpringBootTest
 class BootstrapTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
